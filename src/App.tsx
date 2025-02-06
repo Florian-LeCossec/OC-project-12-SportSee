eea381e8037f9b5e0ed1ee6289dbc7ab90d45256
Florian-LeCossec <florian.le-cossec@orange.fr>
2025-02-06 19:17:16 +0100

    add api service and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import './App.css'
+import { getUserPerformance, getUserActivity, getUserAverageSessions, getUserInfo } from './api/api';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const hasFetchedData = useRef(false);
+
+  useEffect(() => {
+    if (!hasFetchedData.current) {
+      const performance = async () => {
+        try {
+          const performance = await getUserPerformance(18);
+          const activity = await getUserActivity(18);
+          const averageSessions = await getUserAverageSessions(18);
+          const info = await getUserInfo(18);
+
+          console.log('performance', performance);
+          console.log('activity', activity);
+          console.log('averageSessions', averageSessions);
+          console.log('info', info);
+
+          hasFetchedData.current = true;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      void performance();
+    }
+  }, []);
 
   return (
     <>
---
+++ src/api/api.ts
@@ -0,0 +1,122 @@
+import axios from 'axios';
+import mockData from '../data/mockData';
+
+const url = 'http://localhost:3000/user';
+
+
+type UserInfos = {
+    firstName: string;
+    lastName: string;
+    age: number;
+};
+  
+type KeyData = {
+    calorieCount: number;
+    proteinCount: number;
+    carbohydrateCount: number;
+    lipidCount: number;
+};
+  
+type UserData = {
+    id: number;
+    userInfos: UserInfos;
+    todayScore?: number; // optional because sometimes replaced by "score"
+    score?: number; // same reason
+    keyData: KeyData;
+};
+  
+
+export const getUserInfo = async (userId: number): Promise<UserData> => {
+    try {
+        const response = await axios.get<UserData>(`${url}/${userId}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données de ${url}/${userId}:`, error);
+        const userInfo = mockData.USER_MAIN_DATA.find(user => user.id === userId);
+        
+        if (!userInfo) {
+            throw new Error(`Utilisateur avec l'ID ${userId} non trouvé dans les données mock.`);
+        }
+        
+        return userInfo;
+    }
+};
+
+type UserActivity = {
+    userId: number;
+    sessions: {
+        day: string;
+        kilogram: number;
+        calories: number;
+    }[];
+};
+
+export const getUserActivity = async (userId: number): Promise<UserActivity> => {
+    try {
+        const response = await axios.get<UserActivity>(`${url}/${userId}/activity`);
+        return response.data;
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données d'activité de ${url}/${userId}/activity:`, error);
+        const userActivity = mockData.USER_ACTIVITY.find(activity => activity.userId === userId);
+
+        if (!userActivity) {
+            throw new Error(`Activité de l'utilisateur avec l'ID ${userId} non trouvée dans les données mock.`);
+        }
+        return userActivity;
+    }
+};
+
+type UserAverageSessions = {
+    userId: number;
+    sessions: {
+        day: number;
+        sessionLength: number;
+    }[];
+};
+
+export const getUserAverageSessions = async (userId: number): Promise<UserAverageSessions> => {
+    try {
+        const response = await axios.get<UserAverageSessions>(`${url}/${userId}/average-sessions`);
+        return response.data;
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données de moyenne de sessions de ${url}/${userId}/average-sessions:`, error);
+
+        const userAverageSessions = mockData.USER_AVERAGE_SESSIONS.find(session => session.userId === userId);
+
+        if (!userAverageSessions) {
+            throw new Error(`Moyenne de sessions de l'utilisateur avec l'ID ${userId} non trouvée dans les données mock.`);
+        }
+        return userAverageSessions;
+    }
+};
+
+
+type Kind = {
+    [key: number]: string;
+};
+  
+type DataPoint = {
+    value: number;
+    kind: number;
+};
+  
+type UserPerformance = {
+    userId: number;
+    kind: Kind;
+    data: DataPoint[];
+};
+  
+
+export const getUserPerformance = async (userId: number): Promise<UserPerformance> => {
+    try {
+        const response = await axios.get<UserPerformance>(`${url}/${userId}/performance`);
+        return response.data;
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données de performance de ${url}/${userId}/performance:`, error);
+        const userPerformance = mockData.USER_PERFORMANCE.find(performance => performance.userId === userId);
+        if (!userPerformance) {
+            throw new Error(`Performance de l'utilisateur avec l'ID ${userId} non trouvée dans les données mock.`);
+        }
+        return userPerformance;
+    }
+};
